66ce76a1db8e5786e76f1a52434ec7b2146e02c9
Nimo Beeren <nimobeeren@gmail.com>
2022-12-08 15:19:17 +0100

    set specific emojis for projects without an image

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,7 +11,8 @@ export enum ProjectType {
 
 export type Project = {
   image?: string;
-  imageAlt?: string;
+  emoji?: string;
+  alt?: string;
   type: ProjectType;
   title: string;
   body: string;
@@ -23,8 +24,7 @@ export type Project = {
 export const projects: Project[] = [
   {
     image: "/projects/thesis.jpg",
-    imageAlt:
-      "Nimo Beeren and George Fletcher posing at Nimo's graduation ceremony. Nimo is holding his degree.",
+    alt: "Nimo Beeren and George Fletcher posing at Nimo's graduation ceremony. Nimo is holding his degree.",
     type: ProjectType.Thesis,
     title: "Property Graph Schemas",
     body: "In my master's thesis, I formulated a practical and comprehensive schema language for graph databases, which I applied to Neo4j and others.",
@@ -33,6 +33,8 @@ export const projects: Project[] = [
     featured: true,
   },
   {
+    emoji: "📝",
+    alt: "paper and pencil",
     type: ProjectType.Paper,
     title: "Visual Property Graph Schema Design",
     body: "I interviewed people from Google, Uber and TigerGraph with the goal of designing a UI that helps people work with property graph schemas.",
@@ -42,7 +44,7 @@ export const projects: Project[] = [
   },
   {
     image: "/projects/webtex.png",
-    imageAlt: "WebTeX",
+    alt: "WebTeX",
     type: ProjectType.Tool,
     title: "WebTeX",
     body: "Frustrated with LaTeX, I developed a tool for scientific and technical writing based on Markdown. It supports math, citations, and cross-references.",
@@ -52,7 +54,7 @@ export const projects: Project[] = [
   },
   {
     image: "/projects/vr-search.png",
-    imageAlt: "The left half shows a white room with letters scattered over the walls. The right half shows the same room, but the view is obstructed so that only a circular area in the center is visible.",
+    alt: "The left half shows a white room with letters scattered over the walls. The right half shows the same room, but the view is obstructed so that only a circular area in the center is visible.",
     type: ProjectType.Simulation,
     title: "VR Search Experiment",
     body: "We developed and conducted a VR experiment to research how people search for objects in a room.",
@@ -61,7 +63,7 @@ export const projects: Project[] = [
   },
   {
     image: "/projects/dm-guessr.png",
-    imageAlt: "On the left, a photo of a building and a car. On the right, a map with a marker on it.",
+    alt: "On the left, a photo of a building and a car. On the right, a map with a marker on it.",
     type: ProjectType.Game,
     title: "DM-Guessr",
     body: "A game I made to play with the photography association, where you are challenged to guess where our member's photos were taken. Try to beat our high score of 123718!",
@@ -71,7 +73,7 @@ export const projects: Project[] = [
   },
   {
     image: "/projects/birdvis.png",
-    imageAlt: "An interface containing controls to change time and bird species, a map showing temperature anomaly and a map showing bird frequency.",
+    alt: "An interface containing controls to change time and bird species, a map showing temperature anomaly and a map showing bird frequency.",
     type: ProjectType.Visualization,
     title: "Bird Migration",
     body: "We show how temperature is related to the migration pattern of birds in Europe. As global temperatures rise, birds start their breeding season earlier.",
@@ -80,7 +82,7 @@ export const projects: Project[] = [
   },
   {
     image: "/projects/algo-vis.png",
-    imageAlt: "An interface containing controls to change the algorithm, number of machines and jobs, a visualization of the distribution of jobs over machines, and a resulting makespan.",
+    alt: "An interface containing controls to change the algorithm, number of machines and jobs, a visualization of the distribution of jobs over machines, and a resulting makespan.",
     type: ProjectType.Visualization,
     title: "Load Balancing Algorithms",
     body: "This was a fun challenge to implement some algorithms using functional programming patterns in Reason.",
@@ -89,6 +91,8 @@ export const projects: Project[] = [
     featured: true,
   },
   {
+    emoji: "🎧️",
+    alt: "headphones",
     type: ProjectType.Tool,
     title: "spotify-migrate",
     body: "A script I wrote to migrate my local music files to Spotify.",
@@ -97,7 +101,7 @@ export const projects: Project[] = [
   },
   {
     image: "/projects/scrim-valorant.png",
-    imageAlt: "An interface with heading ScrimVALORANT.com. On the left, filters for region, level, maps and post age are shown. In the middle is a list of posts from teams who want to play a scrim match.",
+    alt: "An interface with heading ScrimVALORANT.com. On the left, filters for region, level, maps and post age are shown. In the middle is a list of posts from teams who want to play a scrim match.",
     type: ProjectType.App,
     title: "Valorant Scrim Finder",
     body: "I repurposed my original CS:GO Scrim Finder for the game Valorant.",
@@ -106,7 +110,7 @@ export const projects: Project[] = [
   },
   {
     image: "/projects/css2js.png",
-    imageAlt: "An interface containing vanilla CSS on the left, and JS object style CSS on the right. A dropdown allows changing the CSS style.",
+    alt: "An interface containing vanilla CSS on the left, and JS object style CSS on the right. A dropdown allows changing the CSS style.",
     type: ProjectType.Tool,
     title: "css2js",
     body: "Transforms CSS between various different syntaxes like vanilla CSS, JS objects and JSX props. I enjoyed collaborating with lots of new people in this open-source project!",
@@ -115,6 +119,8 @@ export const projects: Project[] = [
     featured: true,
   },
   {
+    emoji: "🎵",
+    alt: "musical note",
     type: ProjectType.Tool,
     title: "musicdl",
     body: "Automatically download and tag music from YouTube and Spotify.",
@@ -123,7 +129,7 @@ export const projects: Project[] = [
   },
   {
     image: "/projects/drone.png",
-    imageAlt: "A 3D computer simulation of a person following a drone. Some debugging aids are visible.",
+    alt: "A 3D computer simulation of a person following a drone. Some debugging aids are visible.",
     type: ProjectType.Simulation,
     title: "Autonomous Guiding Drone",
     body: "We used computer vision techniques on a virtual camera to simulate a drone which guides people to their destinations.",
@@ -131,6 +137,8 @@ export const projects: Project[] = [
     url: "https://github.com/nimobeeren/0LAUK0",
   },
   {
+    emoji: "🤖",
+    alt: "robot",
     type: ProjectType.Simulation,
     title: "AlphaBeast Draughts Player",
     body: "A program that plays the game of International Draughts, using the alpha-beta pruning algorithm. We had a fun competition with our class.",
@@ -138,6 +146,8 @@ export const projects: Project[] = [
     url: "https://github.com/nimobeeren/InternationalDraughts",
   },
   {
+    emoji: "🌐",
+    alt: "globe",
     type: ProjectType.App,
     title: "CS:GO Scrim Finder",
     body: "My very first React project! It's a web app for finding and organizing competitive matches to play in the game CS:GO, which I played at an amateur level at the time.",
@@ -145,6 +155,8 @@ export const projects: Project[] = [
     url: "https://github.com/nimobeeren/scrim-finder",
   },
   {
+    emoji: "🕹️",
+    alt: "joystick",
     type: ProjectType.App,
     title: "Rescue Robot Operator",
     body: "We built a remote-controlled robot with wheels, some servos and a camera. It is operated through a web interface with gamepad support.",
@@ -153,7 +165,7 @@ export const projects: Project[] = [
   },
   {
     image: "/projects/thermostat.png",
-    imageAlt: "Person holding a phone showing a thermostat app.",
+    alt: "Person holding a phone showing a thermostat app.",
     type: ProjectType.App,
     title: "Thermostat",
     body: "A web interface for a thermostat which supports a weekly schedule.",
@@ -162,7 +174,7 @@ export const projects: Project[] = [
   },
   {
     image: "/projects/study-guide.png",
-    imageAlt: "The hero section of a study guide website.",
+    alt: "The hero section of a study guide website.",
     type: ProjectType.Website,
     title: "TU/e Study Guide",
     body: "A new website containing information about study programs at TU/e. Old tech alert!",
@@ -171,7 +183,7 @@ export const projects: Project[] = [
   },
   {
     image: "/projects/twitch-stats.png",
-    imageAlt: "Website with heading Twitch Stats, a channel picker and a table of common words.",
+    alt: "Website with heading Twitch Stats, a channel picker and a table of common words.",
     type: ProjectType.Visualization,
     title: "Twitch Stats",
     body: "One of my first adventures on the web with JavaScript. This app tracks what's going on in Twitch chat by listing the most common words.",
@@ -179,6 +191,8 @@ export const projects: Project[] = [
     url: "https://github.com/nimobeeren/twitch-stats",
   },
   {
+    emoji: "🎶",
+    alt: "musical notes",
     type: ProjectType.Tool,
     title: "yt-music-dl",
     body: "A command-line program to easily download and tag music from a YouTube playlist.",
