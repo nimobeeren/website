6f81c2de759d047031c9c5e1f3654dcd0a68702c
Nimo Beeren <nimobeeren@gmail.com>
2022-12-13 16:42:07 +0100

    adjust font sizes for mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image()],
 });
