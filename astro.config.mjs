4cd0db8a469b3351156cd36ef833089a9f216df4
Nimo Beeren <nimo.beeren@iodigital.com>
2023-10-27 12:58:10 +0200

    improve labeling of landmarks on mdx pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import image from "@astrojs/image";
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import rehypeSlug from "rehype-slug";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), mdx()],
+  integrations: [
+    tailwind(),
+    image(),
+    mdx({
+      rehypePlugins: [rehypeSlug],
+    }),
+  ],
 });
